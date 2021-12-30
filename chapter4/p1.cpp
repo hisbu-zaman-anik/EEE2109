de0d48c044e9e1298a384db749ad96dcec7685de
hisbu-zaman-anik <hisbuzamananik@gmail.com>
2021-12-31 04:14:11 +0600

    4.1

---
+++ chapter4/p1.cpp
@@ -0,0 +1,29 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    //printing bangladesh with number
+
+
+    //---------------basic for loop stuckture---------
+
+
+//    for (intialization  ; condition ; increment/decrement )
+//   {
+//     statement
+//   }
+//-------------------infine loop-----------------------
+
+
+//    for (  ;  ;  )
+//   {
+//     cout<<"anik";
+//   }
+
+
+        for (int i =1  ; i<=5 ;i++  )
+   {
+     cout << i << "Bangladesh" << endl;
+   }
+}
