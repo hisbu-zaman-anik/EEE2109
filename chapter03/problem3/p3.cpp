4edfc76abc618a75a8381acd73729c85c8e0b1db
hisbu-zaman-anik <hisbuzamananik@gmail.com>
2021-12-31 03:38:00 +0600

    3.3

---
+++ chapter03/problem3/p3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int y;
+   cin>>y;
+   if ((y%400) == 0)
+   {
+     cout<<" leap year ";
+
+   }
+else if ((y%4) == 0 && (y%100)!= 0 )
+{
+    cout<<"leap year ";
+
+}
+ else
+ {
+     cout <<"Not leap year";
+ }
+}
