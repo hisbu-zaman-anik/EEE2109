f3b5b79bb40025a9a134c618d346d888a8a699bc
hisbu-zaman-anik <hisbuzamananik@gmail.com>
2021-12-30 21:13:40 +0600

    1.2

---
+++ Chapter01/problem02/problem02.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    short a1=23;
+    int a2=256;
+    long a3=1389144;
+    long long a4=3259063053;
+
+    float f1=4.00;
+    double f2=3.1415678;
+
+    char c1='a';
+
+    bool b1=true;
+
+    cout<<"value of a1= "<<a1<<endl;
+    cout<<a2<<endl;
+    cout<<a3<<endl;
+    cout<<a4<<endl;
+    cout<<f1<<endl;
+    cout<<f2<<endl;
+    cout<<c1<<endl;
+    cout<<b1<<endl;
+
+
+
+}
