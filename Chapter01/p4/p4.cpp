da0257c9f1cefbba3568dc09c1329d4952c4b625
hisbu-zaman-anik <hisbuzamananik@gmail.com>
2021-12-30 23:03:54 +0600

    1.4

---
+++ Chapter01/p4/p4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//    int b , h , k;
+//    cin>>b>>h;
+//    k=0.5*b*h;
+//    cout<<"Area of Triangle"<<k;
+//      float pi;
+//      cout<<"line 10"<<endl;
+//      cin>>pi;
+//      int r;
+//
+//    float k;
+//     cout<<"line 15"<<endl;
+//      cin>>r;
+//      k=pi*r*r;
+//      cout<<"Area of circle = "<<k;
+        int l, w ,j;
+        cin>>l>>w;
+        j=l*w;
+        cout<<j<<endl;
+
+}
--- Chapter01/problem3/p3.cpp
+++ Chapter01/problem3/p3.cpp
@@ -1,16 +1,1 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int a=23,b=34,sum=0,multipication=1;
-    sum=a+b;
-    multipication=a*b;
-    cout<<"The sum is : "<<sum<<endl;
-    cout<<"Multiplication = "<< multipication;
-    return 0;
 
-
-
-
-
-}
