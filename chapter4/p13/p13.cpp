fd90014313b5eede2e3d50022d76a04d5d945ca0
hisbu-zaman-anik <hisbuzamananik@gmail.com>
2021-12-31 14:53:40 +0600

    4.13

---
+++ chapter4/p13/p13.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int row=3;row>=1;row--)
+
+    {
+        for(int col=1;col<=row;col++)
+        {
+            cout<<row%2;
+        }
+    cout<<endl;
+    }
+
+
+}
