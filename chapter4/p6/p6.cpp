4a73c5bdca69e617c2a17ce979418009648cfb0b
hisbu-zaman-anik <hisbuzamananik@gmail.com>
2021-12-31 13:03:11 +0600

    4.6

---
+++ chapter4/p6/p6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    for(int row=1 ;row<=3 ;row++ )
+    {
+        for(int col=1 ;col<=row ;col++ )
+        {
+            cout<<row%2;
+
+        }
+        cout<<endl;
+    }
+}
