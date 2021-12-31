7b344177f53e7ca412984e55002ddc343456776c
hisbu-zaman-anik <hisbuzamananik@gmail.com>
2021-12-31 13:20:22 +0600

    4.7

---
+++ chapter4/p7/p7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int row=1 ;row<=3 ;row++ )
+    {
+        for(int col=1 ;col<=row ;col++ )
+        {
+            cout<<(char)(col+64);
+        }
+        cout<<endl;
+    }
+}
